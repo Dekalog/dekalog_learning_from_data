1e9bef3bb39c3a3a5d01caf63362704a67da6b4b
Dekalog <Dekalog@users.noreply.github.com>
2014-11-19 01:46:01 +0100

    Create week_6_q_4.m

---
+++ week_6_q_4.m
@@ -0,0 +1,34 @@
+clear all
+
+load in.dta
+load out.dta
+
+% non-linear transforms
+y_in = in(:,3) ;
+in_sample_data = [ ones(size(in,1),1) in(:,1) in(:,2) in(:,1).^2 in(:,2).^2 in(:,1).*in(:,2) abs(in(:,1).-in(:,2)) abs(in(:,1).+in(:,2)) ] ;
+y_out = out(:,3) ; 
+out_sample_data = [ ones(size(out,1),1) out(:,1) out(:,2) out(:,1).^2 out(:,2).^2 out(:,1).*out(:,2) abs(out(:,1).-out(:,2)) abs(out(:,1).+out(:,2)) ] ;
+
+% psuedo-inverse for linear regression
+% w = ( inv( X' * X ) * X' ) * y ; the "classic" way
+% but w = ( ( X' * X ) \ X' ) * y ;
+% is quicker and recommended
+% formula for regularised regression is w = ( ( X' * X + lambda I ) \ X' ) * y ;
+k = 3 ;
+lambda = 10^k ;
+w = ( ( in_sample_data' * in_sample_data + lambda .* eye( size(in_sample_data' * in_sample_data) ) ) \ in_sample_data' ) * y_in ;
+
+% do the classification
+y_in_class = sign( in_sample_data * w ) ;
+y_out_class = sign( out_sample_data * w ) ;
+
+% errors
+e_in = mean( abs( y_in_class .- y_in )./2 ) ;
+e_out = mean( abs( y_out_class .- y_out )./2 ) ;
+
+% euclidean distances
+a = sqrt( (0.2 - e_in)^2 + (0.2 - e_out)^2 )
+b = sqrt( (0.2 - e_in)^2 + (0.3 - e_out)^2 )
+c = sqrt( (0.3 - e_in)^2 + (0.3 - e_out)^2 )
+d = sqrt( (0.3 - e_in)^2 + (0.4 - e_out)^2 )
+e = sqrt( (0.4 - e_in)^2 + (0.4 - e_out)^2 )
